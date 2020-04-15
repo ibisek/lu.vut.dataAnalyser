2d973c8bafb4892bb7f52c7d22ad3bc8801a3849
ibisek <ibisek@protonmail.com>
2020-04-15 17:50:17 +0200

    fixed mistakes in calculations

--- src/dataPreprocessing/dataStandartisation.py
+++ src/dataPreprocessing/dataStandartisation.py
@@ -20,7 +20,7 @@ def standardiseData(dataFrame, originalFileName):
     # flight data: ['NG', 'TQ', 'FF', 'ITT', 'P0', 'PT', 'T0', 'NP']
 
     # temperature compensated to speed:
-    dataFrame['T0C'] = ((dataFrame['T0'] + 273.15) + np.power(dataFrame['TAS'] / 3.6, 2) / (2 * 10004.675)) - 273.15
+    dataFrame['T0C'] = ((dataFrame['T0'] + 273.15) + np.power(dataFrame['TAS'] / 3.6, 2) / (2 * 1004.675)) - 273.15
 
     # ambient pressure compensated to speed:
     dataFrame['P0C'] = dataFrame['P0'] * (1 + np.power(dataFrame['TAS'] / 3.6, 2) / (2 * CONST_R * (dataFrame['T0C'] + 273.15)))
@@ -42,7 +42,7 @@ def standardiseData(dataFrame, originalFileName):
     # dataFrame['TQR'] = dataFrame['TQ'] * (101325 / (dataFrame['P0C']))
 
     # Reduced Shaft Power [W]:
-    dataFrame['SPR'] = dataFrame['SP'] * (101325 / dataFrame['P0C']) * ((15 + 273.15) / (dataFrame['T0C'] + 273.15))
+    dataFrame['SPR'] = dataFrame['SP'] * (101325 / dataFrame['P0C']) * np.sqrt((15 + 273.15) / (dataFrame['T0C'] + 273.15))
 
     # FC / FF / QQ / Qp:
     dataFrame['FCR'] = dataFrame['FC'] * FUEL_DENSITY * (101325 / (dataFrame['P0C'])) * np.sqrt((15 + 273.15) / (273.15 + dataFrame['T0C']))
