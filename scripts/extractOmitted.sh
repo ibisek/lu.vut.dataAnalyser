33c4ba195c3057911afef24d0a9bea191e158804
ibisek <ibisek@protonmail.com>
2020-04-16 10:05:16 +0200

    extraction script

---
+++ scripts/extractOmitted.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+logFile='all-combined.txt'
+path='/home/jaja/data/btsync/doma/radec/out-2020-04-15'
+
+pattern='Omitting; NG = fn (SP)'
+var='ng-sp'
+
+pattern='Omitting; FC = fn (SP)'
+var='fc-sp'
+
+pattern='Omitting; ITT = fn (NG)'
+var='itt-ng'
+
+pattern='Omitting; ITT = fn (SP)'
+var='itt-sp'
+
+
+cat $path/$logFile |grep "$pattern" > $path/$var.txt
+
+cat $path/$var.txt |cut -d'<' -f 2| cut -d'>' -f 1 > $path/$var.csv
+
+
