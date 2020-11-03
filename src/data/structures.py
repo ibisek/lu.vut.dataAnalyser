eb1e7f9c70f10265aba855a0ced6ede8c9cb2c87
ibisek <ibisek@protonmail.com>
2020-11-03 12:08:04 +0100

    +Interval

--- src/data/structures.py
+++ src/data/structures.py
@@ -1,8 +1,12 @@
 
 from enum import Enum
+from collections import namedtuple
 
 
 class RawDataFileFormat(Enum):
     PT6 = 'PT6'
     H80 = 'H80'
+
+
+Interval = namedtuple('Interval', ['start', 'end'])
 
