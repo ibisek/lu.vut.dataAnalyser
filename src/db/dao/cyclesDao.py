9feecb8a672cb3466f2ce82c4c7df0eca3a07f98
ibisek <ibisek@protonmail.com>
2020-11-27 17:03:48 +0100

    py3 super call

--- src/db/dao/cyclesDao.py
+++ src/db/dao/cyclesDao.py
@@ -4,5 +4,5 @@ from db.dao.alchemy import Alchemy
 class CycleDao(Alchemy):
 
     def __init__(self):
-        super(CycleDao, self).__init__()
+        super().__init__()
         self.table = self.base.classes.cycles
