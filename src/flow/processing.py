dff4d75be279a475cb480272828318735e7202ee
ibisek <ibisek@protonmail.com>
2020-11-29 18:43:41 +0100

    max of max and min of min

--- src/flow/processing.py
+++ src/flow/processing.py
@@ -14,6 +14,22 @@ from dao.flightRecordingDao import FlightRecordingDao, RecordingType
 from db.dao.cyclesDao import CyclesDao
 
 
+def _max(a, b):
+    if not a:
+        return b
+    if not b:
+        return a
+    return max(a, b)
+
+
+def _min(a, b):
+    if not a:
+        return b
+    if not b:
+        return a
+    return min(a, b)
+
+
 class Processing:
 
     def __init__(self):
@@ -23,6 +39,24 @@ class Processing:
     def __del__(self):
         self.frDao.influx.stop()
 
+    # @staticmethod
+    # def _checkOverLimitsContravention(self, df: DataFrame, cycle):
+    #     """
+    #     Checks the entire flight for limits contravention.
+    #     :param self:
+    #     :param df:
+    #     :param cycle:
+    #     :return:
+    #     """
+    #     cycle.NGlimL = True
+    #     cycle.NPlimL = True
+    #     cycle.ITTlimL = True
+    #
+    #     cycle.TQlimL = True
+    #     cycle.OilPlimL = True
+    #     cycle.FuelPlimL = True
+    #     cycle.FireWarning = True
+
     @staticmethod
     def _analyseClimbInterval(df: DataFrame, cycle):
         startTs = df.head(1)['ts'][0]
@@ -31,16 +65,16 @@ class Processing:
 
         cycle.BeTimeClim = startTs
         cycle.TimeClim = duration   # TODO is really needed when it can be calculated?
-        cycle.NGRClim = max(df['NG'])
-        cycle.NPClim = max(df['NP'])
-        cycle.TQClim = max(df['TQ'])
-        cycle.ITTClim = max(df['ITT'])
-        cycle.ALTClim = max(df['ALT'])
-        cycle.OilPMinClim = min(df['OILP'])
-        cycle.OilPMaxClim = max(df['OILP'])
-        cycle.OilTMaxClim = max(df['OILT'])
-        # cycle.FuelPMinClim = min(df['X'])     # TODO not in data!
-        # cycle.FuelPMaxClim = max(df['X'])     # TODO not in data!
+        cycle.NGRClim = _max(cycle.NGRClim, max(df['NG']))
+        cycle.NPClim = _max(cycle.NPClim, max(df['NP']))
+        cycle.TQClim = _max(cycle.TQClim, max(df['TQ']))
+        cycle.ITTClim = _max(cycle.ITTClim, max(df['ITT']))
+        cycle.ALTClim = _max(cycle.ALTClim, max(df['ALT']))
+        cycle.OilPMinClim = _min(cycle.OilPMinClim, min(df['OILP']))
+        cycle.OilPMaxClim = _max(cycle.OilPMaxClim, max(df['OILP']))
+        cycle.OilTMaxClim = _max(cycle.OilTMaxClim, max(df['OILT']))
+        # cycle.FuelPMinClim = _min(, min(df['X']))     # TODO not in data!
+        # cycle.FuelPMaxClim = _max(, max(df['X']))     # TODO not in data!
         cycle.EndTimeClim = endTs
 
     @staticmethod
@@ -49,34 +83,34 @@ class Processing:
         endTs = df.tail(1)['ts'][0]
 
         cycle.BeTimeCruis = startTs
-        cycle.NGCruis = max(df['NG'])
-        cycle.NPCruis = max(df['NP'])
-        cycle.TQCruis = max(df['TQ'])
-        cycle.ITTCruis = max(df['ITT'])
-        cycle.AltCruis = max(df['ALT'])
-        cycle.OilPMinCruis = min(df['OILP'])
-        cycle.OilPMaxCruis = max(df['OILP'])
-        cycle.OilTMaxCruis = max(df['OILT'])
-        # cycle.FuelPMinCruis = min(df['X'])     # TODO not in data!
-        # cycle.FuelPMaxCruis = max(df['X'])     # TODO not in data!
+        cycle.NGCruis = _max(cycle.NGCruis, max(df['NG']))
+        cycle.NPCruis = _max(cycle.NPCruis, max(df['NP']))
+        cycle.TQCruis = _max(cycle.TQCruis, max(df['TQ']))
+        cycle.ITTCruis = _max(cycle.ITTCruis, max(df['ITT']))
+        cycle.AltCruis = _max(cycle.AltCruis, max(df['ALT']))
+        cycle.OilPMinCruis = _min(cycle.OilPMinCruis, min(df['OILP']))
+        cycle.OilPMaxCruis = _max(cycle.OilPMaxCruis, max(df['OILP']))
+        cycle.OilTMaxCruis = _max(cycle.OilTMaxCruis, max(df['OILT']))
+        # cycle.FuelPMinCruis = _min(cycle.FuelPMinCruis, min(df['X']))     # TODO not in data!
+        # cycle.FuelPMaxCruis = _max(cycle.FuelPMaxCruis, max(df['X']))     # TODO not in data!
         cycle.EndTimeCruis = endTs
 
     @staticmethod
-    def _analyseIdle(df: DataFrame, cycle):
+    def _analyseIdleInterval(df: DataFrame, cycle):
         startTs = df.head(1)['ts'][0]
         endTs = df.tail(1)['ts'][0]
 
         cycle.BeTimeIdle = startTs
-        # cycle.TimeIdle = 666     # TODO wtf?!
-        # cycle.TimeIdleHyPumpIdle = 666     # TODO wtf?!
-        cycle.NGIdle = max(df['NG'])
-        cycle.ITTIdle = max(df['ITT'])
-        cycle.AltIdle = max(df['ALT'])
-        cycle.OilPMinIdle = min(df['OILP'])
-        cycle.OilPMaxIdle = max(df['OILP'])
-        cycle.OilTMaxIdle = max(df['OILT'])
-        cycle.FuelPMinIdle = min(df['X'])     # TODO not in data!
-        cycle.FuelPMaxIdle = max(df['X'])     # TODO not in data!
+        # cycle.TimeIdle = 666              # TODO wtf?!
+        # cycle.TimeIdleHyPumpIdle = 666    # TODO wtf?!
+        cycle.NGIdle = _max(cycle.NGIdle, max(df['NG']))
+        cycle.ITTIdle = _max(cycle.ITTIdle, max(df['ITT']))
+        cycle.AltIdle = _max(cycle.AltIdle, max(df['ALT']))
+        cycle.OilPMinIdle = _min(cycle.OilPMinIdle, min(df['OILP']))
+        cycle.OilPMaxIdle = _max(cycle.OilPMaxIdle, max(df['OILP']))
+        cycle.OilTMaxIdle = _max(cycle.OilTMaxIdle, max(df['OILT']))
+        # cycle.FuelPMinIdle = _min(cycle.FuelPMinIdle, min(df['X']))     # TODO not in data!
+        # cycle.FuelPMaxIdle = _max(cycle.FuelPMaxIdle, max(df['X']))     # TODO not in data!
         cycle.EndTimeIdle = endTs
 
     def _detectPhases(self, df: DataFrame):
@@ -97,7 +131,7 @@ class Processing:
             dur = (interval.end - interval.start).seconds
             print(f"[INFO] taxi #{i} {interval.start} -> {interval.end}; dur: {dur}s")
 
-        self.engineStartup = detectEngineStartup(df)  # TODO startup-S? can be more than one?
+        self.engineStartup = detectEngineStartup(df)  # TODO startup-S? can there be more than one?
         if self.engineStartup:
             print(f'[INFO] engine startup {self.engineStartup.start} -> {self.engineStartup.end}:', )
 
@@ -122,10 +156,10 @@ class Processing:
             self._analyseClimbInterval(df[climbInterval.start:climbInterval.end], cycle)
 
         for engCruiseInterval in self.engineCruiseIntervals:
-            self._analyseCruise(df[engCruiseInterval.start:engCruiseInterval.end], cycle)
+            self._analyseEngineCruiseInterval(df[engCruiseInterval.start:engCruiseInterval.end], cycle)
 
         for idleInterval in self.engineIdles:
-            self._analyseIdle(df[idleInterval.start:idleInterval.end], cycle)
+            self._analyseIdleInterval(df[idleInterval.start:idleInterval.end], cycle)
 
         print('Y')
         # self.cyclesDao.save()
