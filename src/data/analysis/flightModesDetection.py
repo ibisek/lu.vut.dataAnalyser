69a260d0b1a6fa92031baa3addea00a8756198ce
ibisek <ibisek@protonmail.com>
2020-11-30 17:44:11 +0100

    smaller filtering window

--- src/data/analysis/flightModesDetection.py
+++ src/data/analysis/flightModesDetection.py
@@ -264,7 +264,7 @@ def detectEngineStartups(df: DataFrame) -> List[Interval]:
     while True:
         startIndex = df.loc[df['NG'] > NG_LOW_THR].index[0]  # first index above low thr
         df = df[startIndex:]
-        dNG = df['NG'].diff().rolling(10, center=True).mean()
+        dNG = df['NG'].diff().rolling(5, center=True).mean()
         endIndex = dNG[dNG < 0].head(1).index[0]   # until first derivation is < 0 (non-rising value)
 
         interval = Interval(start=startIndex, end=endIndex)
