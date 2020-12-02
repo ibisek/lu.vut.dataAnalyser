139fe9577440c98917283fcbd21d1d9893e5626c
ibisek <ibisek@protonmail.com>
2020-12-02 18:15:06 +0100

    userDao.py -> usersDao.py

---
+++ src/db/dao/usersDao.py
@@ -0,0 +1,25 @@
+from db.dao.alchemy import Alchemy
+
+
+class UsersDao(Alchemy):
+
+    def __init__(self):
+        super(UsersDao, self).__init__()
+        self.table = self.base.classes.users
+
+    # def getUser(self, login):
+    #     return self.session.query(self.Users).filter_by(login=login)
+
+
+if __name__ == '__main__':
+    ud = UsersDao()
+    # res = d.get(login='pepa')
+    user = ud.getOne(login='pepa', enabled=None)
+    print(user)
+    if user:
+        print(user.__dict__)
+        print("orgid:", user.organisation_id)
+
+        # user.organisation_id += 1
+        # d.session.commit()  # needs to be done manually here or..
+        ud.save()
