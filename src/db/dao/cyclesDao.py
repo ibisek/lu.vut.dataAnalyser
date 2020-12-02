dea6808a68cdec8e9e659bdc36f3a79619cd2fcc
ibisek <ibisek@protonmail.com>
2020-12-02 12:18:45 +0100

    +prepareForSave()

--- src/db/dao/cyclesDao.py
+++ src/db/dao/cyclesDao.py
@@ -1,3 +1,6 @@
+
+import numpy
+
 from db.dao.alchemy import Alchemy
 
 
@@ -6,3 +9,17 @@ class CyclesDao(Alchemy):
     def __init__(self):
         super().__init__()
         self.table = self.base.classes.cycles
+
+    @staticmethod
+    def prepareForSave(cycle):
+        """
+        Converts numpy.float64 values into floats.
+        The database does not understand such values.
+        :param cycle:
+        :return: nix
+        """
+        for attr in cycle.__dict__:
+            val = getattr(cycle, attr)
+
+            if type(val) == numpy.float64:
+                setattr(cycle, attr, float(val))
