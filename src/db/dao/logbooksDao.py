0224999e98388980ddb96d814ea090bed8b63031
ibisek <ibisek@protonmail.com>
2020-12-04 11:25:29 +0100

    fix of problems

--- src/db/dao/logbooksDao.py
+++ src/db/dao/logbooksDao.py
@@ -12,20 +12,20 @@ class LogbooksDao(Alchemy):
 class Logbook:
 
     @staticmethod
-    def add(ts: int, entry: dict, engineId: int = None, airplaneId: int = None, componentId: int = None):
+    def add(ts: int, entry: str, engineId: int = None, airplaneId: int = None, componentId: int = None):
         if not engineId and not airplaneId and not componentId:
-            raise ValueError('At least one of the IDs must be defined!')
+            raise ValueError('At least one of the IDs needs to be defined!')
 
         dao = LogbooksDao()
 
-        entry = dao.createNew()
-        entry.ts = ts
+        rec = dao.createNew()
+        rec.ts = ts
         if engineId:
-            entry.engine_id = engineId
+            rec.engine_id = engineId
         if airplaneId:
-            entry.airplane_id = airplaneId
+            rec.airplane_id = airplaneId
         if componentId:
-            entry.component_id = componentId
-        entry.entry = json.dumps(entry)
+            rec.component_id = componentId
+        rec.entry = entry
 
-        dao.save(entry)
+        dao.save(rec)
