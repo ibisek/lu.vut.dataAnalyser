e995c261f5ec41a233bb1763ee86bb9442d44ad4
ibisek <ibisek@protonmail.com>
2021-01-06 21:27:34 +0100

    the old & traditinal way..

--- src/db/dao/enginesFlightsDao.py
+++ src/db/dao/enginesFlightsDao.py
@@ -1,8 +1,64 @@
 from db.dao.alchemy import Alchemy
 
+from configuration import dbConnectionInfo
+from db.DbSource import DbSource
+
+
+class EngineFlight:
+    engine_id = None
+    flight_id = None
+
+    def __init__(self, engineId=None, flightId=None):
+        self.engine_id = engineId
+        self.flight_id = flightId
+
 
 class EnginesFlightsDao(Alchemy):
 
     def __init__(self):
         super(EnginesFlightsDao, self).__init__()
-        self.table = self.base.classes.engines_flights
+        # self.table = self.base.classes.engines_flights
+
+    def createNew(self) -> EngineFlight:
+        return EngineFlight()
+
+    def save(self, ef: EngineFlight):
+        if not ef.engine_id or not ef.flight_id or self.getOne(engineId=ef.engine_id, flightId=ef.flight_id):
+            return
+
+        sql = f"INSERT INTO engines_flights (engine_id, flight_id) VALUES ({ef.engine_id}, {ef.flight_id})"
+
+        with DbSource(dbConnectionInfo).getConnection() as c:
+            c.execute(sql)
+
+    def getOne(self, engineId: int, flightId: int) -> EngineFlight:
+        if not engineId and not flightId:
+            return None
+
+        with DbSource(dbConnectionInfo).getConnection() as c:
+            sql = f"SELECT engine_id, flight_id FROM engines_flights WHERE "
+            if engineId:
+                sql += f"engine_id = {engineId} "
+                if flightId:
+                    sql += "AND "
+            if flightId:
+                sql += f"flight_id = {flightId}"
+
+            c.execute(sql)
+
+            row = c.fetchone()
+            if row:
+                (enId, flId) = row
+                return EngineFlight(enId, flId)
+
+        return None
+
+
+if __name__ == '__main__':
+    efDao = EnginesFlightsDao()
+    ef = efDao.createNew()
+
+    ef.engine_id = 666
+    ef.flight_id = 667
+
+    efDao.save(ef)
--- src/db/dao/filesFlightsDao.py
+++ src/db/dao/filesFlightsDao.py
@@ -5,12 +5,24 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.automap import automap_base
 
+from configuration import dbConnectionInfo
+from db.DbSource import DbSource
+
+
+class FileFlight:
+    file_id = None
+    flight_id = None
+
+    def __init__(self, fileId=None, flightId=None):
+        self.file_id = fileId
+        self.flight_id = flightId
+
 
 class FilesFlightsDao(Alchemy):
 
     def __init__(self):
         super(FilesFlightsDao, self).__init__()
-        self.table = self.base.classes.files_flights
+        # self.table = self.base.classes.files_flights
 
         # declBase = declarative_base()
         # self.base = automap_base(declBase)
@@ -18,8 +30,46 @@ class FilesFlightsDao(Alchemy):
         #                                    Column('file_id', Integer, ForeignKey('files.id')),
         #                                    Column('flight_id', Integer, ForeignKey('file.id')))
 
+    def createNew(self) -> FileFlight:
+        return FileFlight()
+
+    def save(self, ff: FileFlight):
+        if not ff.file_id or not ff.flight_id or self.getOne(fileId=ff.file_id, flightId=ff.flight_id):
+            return
+
+        sql = f"INSERT INTO files_flights (file_id, flight_id) VALUES ({ff.file_id}, {ff.flight_id})"
+
+        with DbSource(dbConnectionInfo).getConnection() as c:
+            c.execute(sql)
+
+    def getOne(self, fileId: int, flightId: int) -> FileFlight:
+        if not fileId and not flightId:
+            return None
+
+        with DbSource(dbConnectionInfo).getConnection() as c:
+            sql = f"SELECT file_id, flight_id FROM files_flights WHERE "
+            if fileId:
+                sql += f"file_id = {fileId} "
+                if flightId:
+                    sql += "AND "
+            if flightId:
+                sql += f"flight_id = {flightId}"
+
+            c.execute(sql)
+
+            row = c.fetchone()
+            if row:
+                (fiId, flId) = row
+                return FileFlight(fiId, flId)
+
+        return None
+
 
 if __name__ == '__main__':
     ffDao = FilesFlightsDao()
     ff = ffDao.createNew()
-    print(ff)
+
+    ff.file_id = 666
+    ff.flight_id = 667
+
+    ffDao.save(ff)
