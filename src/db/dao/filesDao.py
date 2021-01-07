4e2fd03dfa6f1eef86bfb70d574298f693653627
ibisek <miroslav.cervenka@signia.cz>
2021-01-07 14:25:33 +0100

    a lot of SQL code restored due to Alchemy session troubles

--- src/db/dao/filesDao.py
+++ src/db/dao/filesDao.py
@@ -1,5 +1,4 @@
 from enum import Enum
-from copy import deepcopy
 from sqlalchemy import and_
 
 from db.dao.alchemy import Alchemy
@@ -15,7 +14,7 @@ class FileStatus(Enum):
     UNDER_ANALYSIS = 2
     ANALYSIS_COMPLETE = 3
     # >=128 error states
-    NO_STEADY_STATES = 200    # no steady states detected
+    NO_STEADY_STATES = 200  # no steady states detected
     FAILED = 255
 
     def translate(self, escape_table) -> int:
@@ -26,11 +25,11 @@ class File:
 
     def __init__(self, id, name, raw=False, format=FileFormat.UNDEFINED, status=FileStatus.UNDEF, hash=None):
         self.id = id
-        self.name = name                # original filename
-        self.raw = raw            # 0/1
+        self.name = name  # original filename
+        self.raw = raw  # 0/1
         self.format = format
         self.status = status
-        self.hash = hash          # SHA-256
+        self.hash = hash  # SHA-256
 
     def __str__(self):
         return f"#File: id: {self.id}\n name: {self.name}\n raw: {self.raw}\n format: {self.format}\n status: {self.status}\n hash: {self.hash}"
@@ -54,16 +53,17 @@ class FilesDao(Alchemy):
         #        & (self.base.classes.files.format != FileFormat.UNKNOWN)
         # file = super.getOne(cond)
 
-        q = self.session.query(self.table).filter(and_(self.base.classes.files.status == FileStatus.READY_TO_PROCESS.value,
-                                                       # self.base.classes.files.format != FileFormat.UNDEFINED.value,
-                                                       self.base.classes.files.format != FileFormat.UNKNOWN.value)).limit(1)
+        q = self.session.query(self.table).filter(
+            and_(self.base.classes.files.status == FileStatus.READY_TO_PROCESS.value,
+                 # self.base.classes.files.format != FileFormat.UNDEFINED.value,
+                 self.base.classes.files.format != FileFormat.UNKNOWN.value)).limit(1)
         file = q.first()
 
         if file:
             if file.status is not None:
-                file.status = FileStatus(file.status)   # from int value to object type
+                file.status = FileStatus(file.status)  # from int value to object type
             if file.format is not None:
-                file.format = FileFormat(file.format)   # from int value to object type
+                file.format = FileFormat(file.format)  # from int value to object type
 
         return file
 
@@ -85,6 +85,7 @@ class FilesDao(Alchemy):
     @staticmethod
     def listFiles(engineId: int = None):
         raise NotImplementedError()
+
     #
     #     eidCond = f"WHERE engine_id = {engineId}"
     #
@@ -106,50 +107,62 @@ class FilesDao(Alchemy):
 
     @staticmethod
     def listFilesForNominalCalculation(engineId, limit=20):
-        raise NotImplementedError()
-    #     """
-    #     :param engineId:
-    #     :param limit:
-    #     :return: list of first <limit> files for nominal values calculation
-    #     """
-    #     files = list()
-    #
-    #     with DbSource(dbConnectionInfo).getConnection() as c:
-    #         strSql = f"SELECT id, name, raw, format, status, hash FROM files " \
-    #                  f"WHERE engine_id = {engineId} AND status < 128 " \
-    #                  f"ORDER BY id LIMIT {limit};"
-    #         c.execute(strSql)
-    #
-    #         rows = c.fetchall()
-    #         for row in rows:
-    #             (id, name, flightId, engineId, source, generated, status, hash) = row
-    #             f = File(id=id, name=name, raw=raw, format=FileFormat(format), status=FileStatus(status), hash=hash)
-    #             files.append(f)
-    #
-    #     return files
+        """
+        :param engineId:
+        :param limit:
+        :return: list of first <limit> files for nominal values calculation
+        """
+        files = list()
+
+        with DbSource(dbConnectionInfo).getConnection() as c:
+            sql = f"SELECT f.id, f.name, f.raw, f.format, f.status, f.hash FROM files AS f " \
+                f"JOIN files_flights AS ff ON f.id = ff.file_id " \
+                f"JOIN flights AS fl ON fl.id = ff.flight_id " \
+                f"JOIN engines_flights AS ef ON ef.flight_id = fl.id " \
+                f"WHERE ef.engine_id = {engineId} AND status < 128 " \
+                f"ORDER BY id LIMIT {limit};"
+            c.execute(sql)
+
+            rows = c.fetchall()
+            for row in rows:
+                (id, name, flightId, raw, format, status, hash) = row
+                f = File(id=id, name=name, raw=raw, format=FileFormat(format), status=FileStatus(status), hash=hash)
+                files.append(f)
+
+        return files
 
     @staticmethod
-    def saveFile(file: File):
-        raise NotImplementedError()
-    #     flightId = 'null' if not file.flightId else file.flightId
-    #
-    #     if not file.id:  # new record
-    #         sql = f"INSERT INTO files (name, raw, format, status, hash) " \
-    #               f"VALUES ('{file.name}', {file.raw}, {file.format.value}, {file.status.value}, '{file.hash}');"
-    #
-    #         with DbSource(dbConnectionInfo).getConnection() as c:
-    #             c.execute(sql)
-    #             file.id = c.lastrowid
-    #
-    #     else:   # update existing
-    #         sql = f"UPDATE files SET name='{file.name}', raw={file.raw}, format={file.format.value}, " \
-    #               f"status={file.status.value}, hash='{file.hash}' " \
-    #               f"WHERE id={file.id}"
-    #
-    #         with DbSource(dbConnectionInfo).getConnection() as c:
-    #             c.execute(sql)
-    #
-    #     return file
+    def setFileStatus(file: File, status: FileStatus):
+        if not file or not file.id or not status:
+            return
+
+        with DbSource(dbConnectionInfo).getConnection() as c:
+            sql = f"UPDATE files SET status = {status.value} WHERE id = {file.id};"
+            res = c.execute(sql)
+            if res:
+                return True
+
+        return False
+
+    @staticmethod
+    def save(file: File):
+        if not file.id:  # new record
+            sql = f"INSERT INTO files (name, raw, format, status, hash) " \
+                f"VALUES ('{file.name}', {file.raw}, {file.format.value}, {file.status.value}, '{file.hash}');"
+
+            with DbSource(dbConnectionInfo).getConnection() as c:
+                c.execute(sql)
+                file.id = c.lastrowid
+
+        else:  # update existing
+            sql = f"UPDATE files SET name='{file.name}', raw={file.raw}, format={file.format.value}, " \
+                f"status={file.status.value}, hash='{file.hash}' " \
+                f"WHERE id={file.id}"
+
+            with DbSource(dbConnectionInfo).getConnection() as c:
+                c.execute(sql)
+
+        return file
 
 
 if __name__ == '__main__':
