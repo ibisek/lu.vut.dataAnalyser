a5a30fbd54516e9020f4d2cb229ffb6a76dce453
ibisek <ibisek@protonmail.com>
2021-01-07 08:41:43 +0100

    - duplicate file checking

--- src/loadFilesIntoDb.py
+++ src/loadFilesIntoDb.py
@@ -57,10 +57,10 @@ if __name__ == '__main__':
 
             print("[INFO] file hash:", fileHash)
 
-            file = filesDao.getOne(name=fileName)
-            if file:
-                print(f"[WARN] File '{fileName}' already in DB .. skipping.")
-                continue
+            # file = filesDao.getOne(name=fileName)
+            # if file:
+            #     print(f"[WARN] File '{fileName}' already in DB .. skipping.")
+            #     continue
 
             file = filesDao.createNew()
             file.name = fileName
