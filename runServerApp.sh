8cb795ea656cac0bb86a70dc046102a932ae213d
ibisek <ibisek@protonmail.com>
2021-01-11 11:37:31 +0100

    .

--- runServerApp.sh
+++ runServerApp.sh
@@ -8,8 +8,8 @@ export PYTHONPATH=.:./src:$PYTHONPATH
 
 while true
 do
-  python3 -u ./src/serverApp.py | tee progress.log 2>&1
-  sleep 10
+  python3 -u ./src/serverApp.py | tee -a progress.log 2>&1
+  sleep 15
 done
 
 deactivate
