ae79dbe0cb724c2f5ce98fc5c4d5ee9b5d391f2d
ibisek <ibisek@protonmail.com>
2021-01-14 18:55:13 +0100

    do not loop on failed files

--- src/serverApp.py
+++ src/serverApp.py
@@ -21,28 +21,28 @@ if __name__ == '__main__':
             file.status = FileStatus.FAILED
             filesDao.save(file)
             print(f'[ERROR] Could not migrate file.id {file.id} or the file is not available!')
-            continue
 
-        FileFormatDetector.identify(file)
+        else:
+            FileFormatDetector.identify(file)
 
-        try:
-            file.status = FileStatus.UNDER_ANALYSIS
-            filesDao.save(file)  # TODO uncomment (!)
+            try:
+                file.status = FileStatus.UNDER_ANALYSIS
+                filesDao.save(file)  # TODO uncomment (!)
 
-            engineWorks: List[EngineWork] = preprocess(file)
-            for ew in engineWorks:
-                processing.process(engineWork=ew)
+                engineWorks: List[EngineWork] = preprocess(file)
+                for ew in engineWorks:
+                    processing.process(engineWork=ew)
 
-            file.status = FileStatus.ANALYSIS_COMPLETE
-            print(f'[INFO] Work for file id {file.id} finished.')
+                file.status = FileStatus.ANALYSIS_COMPLETE
+                print(f'[INFO] Work for file id {file.id} finished.')
 
-        except Exception as ex:
-            print(f"[ERROR] in processing file id {file.id}:", str(ex))
-            traceback.print_exc(file=sys.stdout)
-            file.status = FileStatus.FAILED
+            except Exception as ex:
+                print(f"[ERROR] in processing file id {file.id}:", str(ex))
+                traceback.print_exc(file=sys.stdout)
+                file.status = FileStatus.FAILED
 
-        finally:
-            filesDao.save(file)  # TODO uncomment (!)
+            finally:
+                filesDao.save(file)  # TODO uncomment (!)
 
         # get next work assignment:
         file: File = checkForWork()
