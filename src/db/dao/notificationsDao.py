8921f5bf1fece42029490b4669c299190848e431
ibisek <ibisek@protonmail.com>
2021-01-22 18:57:36 +0100

    +list() fn

--- src/db/dao/notificationsDao.py
+++ src/db/dao/notificationsDao.py
@@ -8,9 +8,18 @@ class NotificationsDao(Alchemy, Singleton):
         super(NotificationsDao, self).__init__()
         self.table = self.base.classes.notifications
 
+    def listMostRecent(self, limit=10):
+        query = self.session.query(self.table).order_by(self.table.id.desc()).limit(limit)
+
+        return query.all()
+
 
 if __name__ == '__main__':
     notificationsDao = NotificationsDao()
 
     n = notificationsDao.createNew()
+
+    notifications = notificationsDao.listMostRecent()
+    for n in notifications:
+        print("n:", n)
 
